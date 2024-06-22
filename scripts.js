f9f22ae3259f0b66ce211eed81f74785fa44b3cc
Davi André <161468196+da6zin@users.noreply.github.com>
2024-06-21 22:31:57 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
-// Função para abrir o menu lateral
 document.getElementById('menu-button').addEventListener('click', function() {
     document.getElementById('sidebar').style.width = '250px';
 });
 
-// Função para fechar o menu lateral
+
 document.getElementById('close-sidebar').addEventListener('click', function() {
     document.getElementById('sidebar').style.width = '0';
 });
 
-// Redirecionar para a página inicial ao clicar no ícone de casa
+
 document.getElementById('home-button').addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = 'index.html';  // Troque 'index.html' pelo caminho correto do seu arquivo
+    window.location.href = 'index.html'; 
 });
 
 
@@ -20,5 +19,5 @@ document.getElementById('pending-projects').addEventListener('click', function()
 });
 
 document.getElementById('accepted-projects').addEventListener('click', function() {
-    window.location.href = 'https://da6zin.github.io/Juliana-ProjetosAceitos/';  // Troque pelo caminho correto do seu arquivo
+    window.location.href = 'https://da6zin.github.io/Juliana-ProjetosAceitos/'; 
 });
